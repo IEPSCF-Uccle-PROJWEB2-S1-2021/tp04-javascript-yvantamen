bbb446cf58fb0410ed3cec9e064c5a7a27988ca9
yvan <yvantamen@yahoo.com>
2020-10-05 21:37:17 +0200

    max

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function Min(a,b)
 }
 let resultMin=Min(15,15);
 console.log('le minimum est'+resultMin); 
+
+
+//fonctionMax
+function Max(a,b)
+{
+    return Math.max(a,b);
+}
+let resultMax=Max(30,15);
+console.log('le maximum est'+resultMax);
+
